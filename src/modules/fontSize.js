2ec5689a0cd9f6fa2ba45ebb095f96c23625cd11
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-04-19 10:55:58 +0700

    change fontSizeByLength formula
    with new formula, min font size will be larger but max will be the same (2.25 rem)

--- src/modules/fontSize.js
+++ src/modules/fontSize.js
@@ -1,5 +1,7 @@
 export default function fontSize(string) {
     const n = string.length;
-    const calculated = Math.round((2.25 / Math.log(n + 1.72)) * 100) / 100;
+    const calculated =
+        Math.round((2.25 / Math.log((n + 1.71828) / 0.85) + 0.315) * 100) / 100;
+
     return `${calculated}rem`;
 }
